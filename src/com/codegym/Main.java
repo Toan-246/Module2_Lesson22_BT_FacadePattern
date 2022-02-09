9f185c186c58cc4bcd4a52a523aa550994804a38
Toan-246 <n.v.toan687@gmail.com>
2022-02-09 16:02:22 +0700

    âdd

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -3,6 +3,7 @@ package com.codegym;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        RandomListFacade randomListFacade = new RandomListFacade();
+        randomListFacade.printRandomEvenList(100,0,100);
     }
 }
